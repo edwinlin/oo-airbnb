7fdb09738d7f9d86a38fb1b620b29a14ba43fdb8
edwinlin <edwinlin@gmail.com>
2019-01-14 15:22:34 -0500

    crushed it

--- tools/console.rb
+++ tools/console.rb
@@ -4,22 +4,16 @@ def reload
   load 'config/environment.rb'
 end
 
-
-
-
-
 # Put your variables here~!
 guest1 = Guest.new("bob")
 guest2 = Guest.new("bobby")
 guest3 = Guest.new("Robert")
 guest4 = Guest.new("bobby")
 
-
 listing1 = Listing.new("Seattle")
 listing2 = Listing.new("DC")
 listing3 = Listing.new("NY")
 listing4 = Listing.new("NY")
-
 
 trip1 = Trip.new(guest1, listing1, 10)
 trip2 = Trip.new(guest2, listing2, 10)
