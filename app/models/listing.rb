c7314d65faf4da063c7fcd29ae10bff3f0a82f9e
edwinlin <edwinlin@gmail.com>
2019-01-14 15:16:17 -0500

    done

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,1 +1,41 @@
+class Listing
+	attr_reader :name
+	@@all = []
 
+	def initialize(name)
+		@name = name
+		@@all << self
+	end
+
+	def guests
+		trips.map do |trip|
+			trip.guest
+		end
+	end
+
+	def trips
+		Trip.all.select do |trip|
+			trip.listing == self
+		end
+	end
+
+	def trip_count
+		trips.length
+	end
+
+	def self.all
+		@@all
+	end
+
+	def self.find_all_by_city(city)
+		self.all.select do |listing|
+			listing.name == city
+		end
+	end
+
+	def self.most_popular
+		@@all.sort_by do |listing|
+			listing.trip_count
+		end[-1]
+	end
+end
--- tools/console.rb
+++ tools/console.rb
@@ -9,4 +9,20 @@ end
 
 
 # Put your variables here~!
+guest1 = Guest.new("bob")
+guest2 = Guest.new("bobby")
+guest3 = Guest.new("Robert")
+guest4 = Guest.new("bobby")
+
+
+listing1 = Listing.new("Seattle")
+listing2 = Listing.new("DC")
+listing3 = Listing.new("NY")
+listing4 = Listing.new("NY")
+
+
+trip1 = Trip.new(guest1, listing1, 10)
+trip2 = Trip.new(guest2, listing2, 10)
+trip3 = Trip.new(guest2, listing2, 10)
+
 binding.pry
